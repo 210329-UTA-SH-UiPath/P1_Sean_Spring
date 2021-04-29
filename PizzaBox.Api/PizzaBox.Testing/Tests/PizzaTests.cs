0f34dd71f80a14c104cb5da51bd7c5dd63b98a81
sspring963 <sspring963@gmail.com>
2021-04-29 10:25:08 -0500

    added database diagram

--- PizzaBox.Api/PizzaBox.Testing/Tests/PizzaTests.cs
+++ PizzaBox.Api/PizzaBox.Testing/Tests/PizzaTests.cs
@@ -5,10 +5,48 @@ using Xunit;
 
 namespace PizzaBox.Testing.Tests
 {
-
+    
     public class PizzaTests
     {
 
+        Pizza pizza = new Pizza
+        {
+            Name = "Name",
+            PizzaId = 1,
+            SizeId = 1,
+            CrustId =1
+        };
+
+        [Fact]
+        public void TestPizzaId()
+        {
+
+            var sut = pizza.PizzaId;
+
+            bool istwo = sut == 1;
+
+            Assert.True(istwo);
+        }
+        [Fact]
+        public void TestCrustId()
+        {
+
+            var sut = pizza.CrustId;
+
+            bool istwo = sut == 1;
+
+            Assert.True(istwo);
+        }
+        [Fact]
+        public void TestSizeId()
+        {
+
+            var sut = pizza.SizeId;
+
+            bool istwo = sut == 1;
+
+            Assert.True(istwo);
+        }
         //[Fact]
         //public void TestName()
         //{
